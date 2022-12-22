9c2b4af868b20d9aee95350de1be475bc6582144
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2022-12-23 00:44:18 +0530

    Time: 5 ms (22.18%), Space: 7.4 MB (39.34%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -4,15 +4,15 @@ public:
     
     void solve(string s, unordered_set<string> &wordset, string res){
 
-        if(s.size()==0) 
+        if(s.size() == 0) 
             return;
 		
         if(wordset.count(s) == true)
              ans.push_back(res+s);
         
-		// we do not return from function as there might be some substring possible which         might be part of another answer.
+		// we do not return from function as there might be some substring possible which might be part of another answer.
         
-		int n=s.size();
+		int n = s.size();
 
         for(int i=0; i<n; ++i){
 
