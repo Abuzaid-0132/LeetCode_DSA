53b5c3a9fd3fb129fd9a7430bbd6f1784caec82b
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2022-12-22 22:04:33 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -11,53 +11,53 @@ using namespace std;
 class Solution{
     public:
     
-    void findPathHelper(int i, int j, vector < vector < int >> & a, int n, vector < string > & ans, string move,
-    vector < vector < int >> & vis) {
-    if (i == n - 1 && j == n - 1) {
-      ans.push_back(move);
-      return;
+    void solve(int i,int j, vector<vector<int>> &matrix, int n, vector<string> &ans, string move, vector<vector<int>> &visited) {
+        
+        if(i == n-1 && j == n-1) {
+            ans.push_back(move);
+            return;
+        }
+        
+        // downward
+        if(i+1 < n && visited[i+1][j] == 0 && matrix[i+1][j] == 1) {
+            visited[i][j] = 1;
+            solve(i+1,j,matrix,n,ans,move+'D',visited);
+            visited[i][j] = 0;
+        }
+        
+        // left
+        if(j-1 >= 0 && visited[i][j-1] == 0 && matrix[i][j-1] == 1) {
+            visited[i][j] = 1;
+            solve(i,j-1,matrix,n,ans,move+'L',visited);
+            visited[i][j] = 0;
+        }
+        
+        // right
+        if(j+1 < n && visited[i][j+1] == 0 && matrix[i][j+1] == 1) {
+            visited[i][j] = 1;
+            solve(i,j+1,matrix,n,ans,move+'R',visited);
+            visited[i][j] = 0;
+        }
+        
+        // upward
+        if(i-1 >= 0 && visited[i-1][j] == 0 && matrix[i-1][j] == 1) {
+            visited[i][j] = 1;
+            solve(i-1,j,matrix,n,ans,move+'U',visited);
+            visited[i][j] = 0;
+        }
+            
     }
-
-    // downward
-    if (i + 1 < n && !vis[i + 1][j] && a[i + 1][j] == 1) {
-      vis[i][j] = 1;
-      findPathHelper(i + 1, j, a, n, ans, move + 'D', vis);
-      vis[i][j] = 0;
-    }
-
-    // left
-    if (j - 1 >= 0 && !vis[i][j - 1] && a[i][j - 1] == 1) {
-      vis[i][j] = 1;
-      findPathHelper(i, j - 1, a, n, ans, move + 'L', vis);
-      vis[i][j] = 0;
-    }
-
-    // right 
-    if (j + 1 < n && !vis[i][j + 1] && a[i][j + 1] == 1) {
-      vis[i][j] = 1;
-      findPathHelper(i, j + 1, a, n, ans, move + 'R', vis);
-      vis[i][j] = 0;
-    }
-
-    // upward
-    if (i - 1 >= 0 && !vis[i - 1][j] && a[i - 1][j] == 1) {
-      vis[i][j] = 1;
-      findPathHelper(i - 1, j, a, n, ans, move + 'U', vis);
-      vis[i][j] = 0;
-    }
-
-  }
     
     
-    vector<string> findPath(vector<vector<int>> &m, int n) {
+    vector<string> findPath(vector<vector<int>> &matrix, int n) {
         
-      vector < string > ans;
-      vector < vector < int >> vis(n, vector < int > (n, 0));
-
-      if (m[0][0] == 1) 
-        findPathHelper(0, 0, m, n, ans, "", vis);
+        vector<string> ans;
+        vector<vector<int>> visited(n,vector<int> (n,0));
         
-      return ans;
+        if(matrix[0][0] == 1)
+            solve(0,0,matrix,n,ans,"",visited);
+            
+        return ans;
     }
 };
 
