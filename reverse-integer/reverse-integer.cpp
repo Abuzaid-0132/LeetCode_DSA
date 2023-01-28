79d9451c36ae37ac3bbe804b68ba3506735c88a5
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-01-28 15:15:05 +0530

    Time: 7 ms, Memory: 6 MB - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int reverse(int x) {
+        // Convert into positive
+        long long int t = abs(x);
+        
+        long long int rev = 0;
+        
+        // This is code for reversing the number
+        while(t != 0){
+            
+            // Store Last didgit in -> a
+            int a = t % 10;
+            
+            // Add it with * 10 so the number get's reversed
+            rev = (rev * 10) + a;
+            
+            t = t/10;
+            
+        }
+        
+        // Condition given in the Question
+        if(rev > INT_MAX || rev < INT_MIN)
+        {
+            return 0;
+        }
+        
+        // Since we converted into positive before
+        // Now check and result it appropriately
+        if(x >= 0){
+            return rev;
+        }
+        
+        else{
+            return -rev;
+        }
+    }
+};
