232ff3c1658bc0552f08effd3a4743b1da32445d
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-01-28 12:01:02 +0530

    Time: 41 ms, Memory: 12.1 MB - LeetHub

--- 0two-sum/0two-sum.cpp
+++ 0two-sum/0two-sum.cpp
@@ -4,12 +4,20 @@ public:
         
         vector<int> ans;
         
-        for(int i=0; i<nums.size(); i++)
-            for(int j=i+1; j<nums.size(); j++)
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+        unordered_map<int,int> map;
+        
+        for(int i=0; i<nums.size(); i++) {
+            
+            if(map.find(target - nums[i]) != map.end()) {
+                
+                ans.push_back(map[target - nums[i]]);
+                ans.push_back(i);
+                
+            }
+            
+            map[nums[i]] = i;
+            
+        }
         
         return ans;
     }
