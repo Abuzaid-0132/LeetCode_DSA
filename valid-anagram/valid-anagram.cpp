3988588dfeea831ba19c0b23fc0401ace861ba00
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-01-28 19:12:45 +0530

    Time: 3 ms, Memory: 7.3 MB - LeetHub

---
+++ valid-anagram/valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.length() != t.length())
+            return false;
+        
+        int count[256] = {0};  // no_of_char = 256.
+        
+        for(int i=0; i<s.length(); i++) {
+            
+            count[s[i]]++;
+            count[t[i]]--;
+            
+        }
+        
+        for(int i=0; i<256; i++)
+            if(count[i] != 0)
+                return false;
+        
+        return true;
+        
+    }
+};
