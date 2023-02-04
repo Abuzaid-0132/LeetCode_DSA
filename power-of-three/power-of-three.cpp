63668ac1881926482f3ca3024f3da642b153b4e9
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-02-04 20:30:28 +0530

    Time: N/A, Memory: N/A',
      },
      questionId: '326',
      submissionId: '891359166',
      sessionId: '4157768',
      getLangDisplay: 'cpp',
      submissionCode: 'class Solution {\u000Apublic:\u000A    bool isPowerOfThree(int n) {\u000A        \u000A        while(n \u003E\u003D 3) {\u000A            \u000A            if(n % 3 !\u003D 0)\u000A                return false\u003B\u000A            \u000A            n \u003D n / 3\u003B\u000A            \u000A        }\u000A        \u000A        return n \u003D\u003D 1 ? true : false\u003B\u000A    }\u000A}\u003B',
      editCodeUrl: '/explore/item/745',
      checkUrl: '/submissions/detail/891359166/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022cpp\u0022, \u0022distribution\u0022: [[\u00220\u0022, 0.0848], [\u00223\u0022, 0.2121], [\u00224\u0022, 0.2404], [\u00227\u0022, 0.7776], [\u00228\u0022, 0.919], [\u00229\u0022, 0.0141], [\u002210\u0022, 0.0141], [\u002211\u0022, 2.3894], [\u002212\u0022, 2.3894], [\u002214\u0022, 0.0707], [\u002215\u0022, 4.1425], [\u002216\u0022, 4.3687], [\u002217\u0022, 0.1414], [\u002218\u0022, 0.2121], [\u002219\u0022, 6.1643], [\u002220\u0022, 5.8108], [\u002221\u0022, 0.3959], [\u002222\u0022, 0.4807], [\u002223\u0022, 6.546], [\u002224\u0022, 6.3057], [\u002225\u0022, 0.6504], [\u002226\u0022, 0.9473], [\u002227\u0022, 5.6694], [\u002228\u0022, 5.3443], [\u002229\u0022, 0.7635], [\u002230\u0022, 1.3431], [\u002231\u0022, 4.1708], [\u002232\u0022, 4.2415], [\u002233\u0022, 0.8766], [\u002234\u0022, 1.3856], [\u002235\u0022, 2.8559], [\u002236\u0022, 3.0963], [\u002237\u0022, 0.7635], [\u002238\u0022, 1.1735], [\u002239\u0022, 2.0925], [\u002240\u0022, 1.6683], [\u002241\u0022, 0.6645], [\u002242\u0022, 0.7069], [\u002243\u0022, 1.6542], [\u002244\u0022, 1.5128], [\u002245\u0022, 0.3535], [\u002246\u0022, 0.7352], [\u002247\u0022, 1.0886], [\u002248\u0022, 0.919], [\u002249\u0022, 0.5373], [\u002250\u0022, 0.6362], [\u002251\u0022, 0.9331], [\u002252\u0022, 0.9473], [\u002253\u0022, 0.4807], [\u002254\u0022, 0.5514], [\u002255\u0022, 0.9755], [\u002256\u0022, 0.5938], [\u002257\u0022, 0.4383], [\u002258\u0022, 0.5373], [\u002259\u0022, 0.7352], [\u002260\u0022, 0.311], [\u002261\u0022, 0.2262], [\u002262\u0022, 0.2828], [\u002263\u0022, 0.3535]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022cpp\u0022, \u0022distribution\u0022: [[\u00225600\u0022, 0.0848], [\u00225700\u0022, 3.577], [\u00225800\u0022, 13.799], [\u00225900\u0022, 20.3308], [\u00226000\u0022, 42.8107], [\u00226100\u0022, 5.9098], [\u00226200\u0022, 7.2529]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

---
+++ power-of-three/power-of-three.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        while(n >= 3) {
+            
+            if(n % 3 != 0)
+                return false;
+            
+            n = n / 3;
+            
+        }
+        
+        return n == 1 ? true : false;
+    }
+};
