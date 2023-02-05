f9523cb5ad54a2336d2700360c9f96a469353c5b
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-02-05 14:44:24 +0530

    Time: 16 ms, Memory: 17.9 MB - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        int total_sum = n * (n + 1) / 2;
+        
+        int sum = 0;
+        
+        for(int i=0; i<nums.size(); i++)
+            sum += nums[i];
+        
+        return total_sum - sum;
+        
+    }
+};
