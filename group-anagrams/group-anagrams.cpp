174721af850d776aab870cba7849908fef8ee501
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-02-06 15:25:52 +0530

    Time: 40 ms, Memory: 20.8 MB - LeetHub

---
+++ group-anagrams/group-anagrams.cpp
@@ -0,0 +1,37 @@
+	
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+       vector<vector<string>>ans;
+        
+       unordered_map<string, vector<string>> map;
+        
+         /*
+                Consider example 1 : strs = ["eat","tea","tan","ate","nat","bat"]
+                
+                After the below opeartion of for loop map will contain
+                
+                aet -- eat, tea, ate
+                ant -- tan, nat
+                abt -- bat
+        */
+        
+        for(int i = 0; i < strs.size(); i++) {
+            
+            string s = strs[i];
+            sort(strs[i].begin(),strs[i].end());
+            map[strs[i]].push_back(s);
+        }
+        
+        
+        //now simply put the elements  of second column of map in ans
+        for(auto it : map){
+            
+            ans.push_back(it.second);
+        }
+
+        return ans;
+        
+    }
+};
