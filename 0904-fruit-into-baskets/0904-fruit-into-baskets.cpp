3c9c2b6a68e9713e7a89173a5568f83356531d5c
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-02-07 11:12:44 +0530

    Time: 191 ms (60.15%), Space: 71.3 MB (48.83%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -4,18 +4,18 @@ public:
         
         unordered_map<int, int> map;
         
-        int i = 0;
+        int i = 0; 
         int j = 0;
-        int ans = 0;
+        int result = 0;
         
         while(j < fruits.size()) {
             
             map[fruits[j]]++;
             
             if(map.size() <= 2)
-                ans = max(ans, j-i+1);
+                result = max(result, j-i+1);
             
-            else {
+            else{
                 
                 map[fruits[i]]--;
                 
@@ -27,6 +27,7 @@ public:
             
             j++;
         }
-        return ans;
+     
+        return result;
     }
 };
