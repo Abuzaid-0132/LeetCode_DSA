e0119139e53c725b7dcfee4ce1de18f792d30598
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-02-10 08:00:57 +0530

    Added solution - LeetHub

---
+++ Balloon Everywhere - GFG/balloon-everywhere.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+     int maxInstance(string s){
+         
+        unordered_map<char, int> check;
+        string S = "balloon";
+        
+        for(auto s1: S)
+            check[s1] = 0;
+        
+        
+        for(auto s1: s){
+            
+            if(s1 == 'b'|| s1 == 'a'|| s1 == 'l'|| s1 == 'o'|| s1 == 'n')
+                 check[s1]++;
+            
+        }
+        
+        check['l'] = check['l']/2;
+        check['o'] = check['o']/2;
+        
+        int ans = INT_MAX;
+        
+        for(auto x: check)
+            ans = min(ans, x.second);
+        
+        if(ans <= 0)
+            return 0;
+        
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.maxInstance(s)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
