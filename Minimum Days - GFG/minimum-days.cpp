66a4337a926ce8881d4ad674aff623c1c9909478
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-02-11 11:07:21 +0530

    Added solution - LeetHub

---
+++ Minimum Days - GFG/minimum-days.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getMinimumDays(int n,string s, vector<int> &p) {
+        // code here.
+        int ans = 0;
+        
+        for(int i=0; i<n; i++) {
+            
+            int idx = p[i];
+            
+            if((idx > 0 && s[idx] == s[idx-1]) || (idx+1 < n && s[idx] == s[idx+1])) {
+                
+                s[idx] = '?';
+                ans = i+1;
+                
+            }
+            
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        string S;cin>>S;
+        
+        vector<int> P(N);
+        Array::input(P,N);
+        
+        Solution obj;
+        int res = obj.getMinimumDays(N,S, P);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
