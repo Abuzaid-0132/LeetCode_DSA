dca82a73b584f1eab0c1c38361995f705ff55293
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-02-16 18:02:02 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -9,21 +9,22 @@ class Solution{
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
-    long long merge(vector<long long>&nums, int low,int mid,int high)
-    {
+    long long merge(vector<long long>&nums, int low,int mid,int high) {
+        
         long long inv_count=0, k=0;
         
         int l = low, r = mid+1, size = high-low+1;
         
         vector<long long>sorted(size);
         
-        while(l <= mid and r <= high)
-        {
-            if(nums[l] <= nums[r]){
+        while(l <= mid and r <= high) {
+            
+            if(nums[l] <= nums[r]) {
+                
                 sorted[k++] = nums[l++];
             }
-            else
-            {
+            else {
+                
                 sorted[k++] = nums[r++];
                 inv_count = inv_count + (mid-l+1); // only these changes
             }
@@ -38,14 +39,16 @@ class Solution{
         for(k = 0 ; k < size ; k++)
             nums[k+low] = sorted[k];
             
+            
         return inv_count;
     }
     
-    long long mergeSort(vector<long long>&nums,int low,int high)
-    {
+    long long mergeSort(vector<long long>&nums,int low,int high) {
+        
         long long inv_count = 0;
         
-        if(low >= high) return 0;
+        if(low >= high) 
+            return 0;
         
         long long mid = low + (high-low)/2;
         
@@ -58,12 +61,13 @@ class Solution{
         return inv_count;
     }
     
-    long long int inversionCount(long long arr[], long long n)
-    {
+    long long int inversionCount(long long arr[], long long n) {
+        
         vector<long long>nums;
         
         for(int i=0;i<n;i++) 
             nums.push_back(arr[i]);
+            
             
         return mergeSort(nums, 0, n-1);   
     }
