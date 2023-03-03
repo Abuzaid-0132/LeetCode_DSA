c3f305fc618e4bf37dbe8760d8e92ba4aff31626
Abuzaid <97349697+Abuzaid-0132@users.noreply.github.com>
2023-03-03 10:27:35 +0530

    Time: 5 ms (39.03%), Space: 12.1 MB (79.11%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -13,46 +13,54 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         
-       vector<vector<int>> ans;
+        vector<vector<int>> ans;
         
         if(root == NULL)
             return ans;
         
+        if(root->left == NULL && root->right == NULL) {
+            
+            ans.push_back({root->val});
+            return ans;
+        }
+            
         queue<TreeNode*> q;
         q.push(root);
         
         int level = 0;
         
-        while(q.empty() == false) {
-            
-            int size = q.size();
-            vector<int> temp;
-            
-            for(int i=0; i<size; i++) {
-                
-                TreeNode* curr = q.front();
-                q.pop();
-                
-                temp.push_back(curr->val);
-                
-                if(curr->left)
-                    q.push(curr->left);
-                
-                if(curr->right)
-                    q.push(curr->right);
-                
-            }
-            
-            if(level % 2 == 0)
-                ans.push_back(temp);
-            else{
-                reverse(temp.begin(), temp.end());
-                ans.push_back(temp);
-            }
-                
-            level++;
-            
-        }
+         while(q.empty() == false) {
+             
+             vector<int> vec;
+             
+             int size = q.size();
+             
+             for(int i=0; i<size; i++) {
+                 
+                 TreeNode* curr = q.front();
+                 q.pop();
+                 
+                 vec.push_back(curr->val);
+                 
+                 if(curr->left)
+                     q.push(curr->left);
+                 
+                 if(curr->right)
+                     q.push(curr->right);
+                 
+             }
+             
+             if(level % 2 == 0)
+                ans.push_back(vec);
+             
+             else{
+                 
+                 reverse(vec.begin(), vec.end());
+                 ans.push_back(vec);
+             }
+             
+             level++;
+         }
         
         return ans;
     }
